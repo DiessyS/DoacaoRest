d3a0737ae7b43c0cfd57d82e08a2869a56683261
DiessyS <diessy@alunos.utfpr.edu.br>
2022-09-29 22:56:38 -0300

    melhoria_geracao_token

--- src/main/groovy/doacaorest/DToken.groovy
+++ src/main/groovy/doacaorest/DToken.groovy
@@ -1,16 +1,20 @@
 package doacaorest
 
+import java.text.SimpleDateFormat
+
 class DToken {
-    static private SERVER_SEED = "9L0pLQsoOEOv2K52I1URy1Dbe5zP6JjZTpRs8edvGN2T6QLpaKBxvNpcfLBkCbf"
+    static private SERVER_SEED = UUID.randomUUID().toString()
 
     static def generateTokenUsuario(idUsuario) {
         def usuario = Usuario.get(idUsuario)
 
-        if(usuario == null) {
-            return null
-        }
+        if(usuario == null) { return null }
 
-        return ((usuario.cpf + usuario.senha).sha256() + SERVER_SEED).sha256()
+        def sdf = new SimpleDateFormat("MM/dd/yyyy")
+
+        return (
+            usuario.cpf + sdf.format(new Date()) + usuario.senha + SERVER_SEED
+        ).digest('SHA-1')
     }
 
     static def validateTokenUsuario(idUsuario, token) {
